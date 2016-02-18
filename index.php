23e69663707aba31cd801c09aeeb3a8581adbc39
Aryan Cheema <aryan.cheema.152@my.aquinas.me>
2016-02-17 20:22:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 include 'core/init.php';
 include 'includes/overall/header.php';
 ?>
-<h1>Home Page</h1>
-<p>Hello and Welcome! Click the button on the right widget to register now.</p>
+  <h1>Home Page</h1>
+  <p>Hello and Welcome! Click the button on the right widget to register now.</p>
 
 
 
