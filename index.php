093ac92f83b2da4e842ae1d026bec39827f17eb9
Aryan Cheema <aryan.cheema.152@my.aquinas.me>
2016-02-17 20:22:07 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+include 'core/init.php';
+include 'includes/overall/header.php';
+?>
+<h1>Home Page</h1>
+<p>Hello and Welcome! Click the button on the right widget to register now.</p>
+
+
+
+
+
+<?php include 'includes/footer.php'; ?>
