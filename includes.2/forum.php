0e6790ed4916c63f4c644316a117e484a62bed2e
Aryan Cheema <aryan.cheema.152@my.aquinas.me>
2016-03-08 21:35:11 -0500

    Create forum.php

---
+++ includes.2/forum.php
@@ -0,0 +1,15 @@
+<?php 
+include 'core/init.php';
+protect_page();
+include 'includes/overall/header.php';
+?>
+<h1>Forum</h1>
+<p>St. Thomas Aquinas Tutoring. </p>
+<p>Hello and Welcome. Click on the Register button to the right to Register now!</p>
+
+
+
+
+
+<?php include 'includes/footer.php'; ?>
+
