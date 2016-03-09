dacded58de19fdd05589d55ba9eef6c7586e3cb4
Aryan Cheema <aryan.cheema.152@my.aquinas.me>
2016-03-08 21:35:27 -0500

    Create protected.php

---
+++ includes.2/protected.php
@@ -0,0 +1,14 @@
+<?php 
+include 'core/init.php';
+include 'includes/overall/header.php';
+?>
+<h1>Sorry you need to be logged in to do that!</h1>
+<p>Please register or log in by using the widget on the right. </p>
+
+
+
+
+
+
+<?php include 'includes/footer.php'; ?>
+
