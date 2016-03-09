fa78f5846d8702df4fd417c74200f1de4c2a8a5e
Aryan Cheema <aryan.cheema.152@my.aquinas.me>
2016-03-08 21:34:58 -0500

    Create downloads.php

---
+++ includes.2/downloads.php
@@ -0,0 +1,12 @@
+<?php 
+
+include 'core/init.php';
+protect_page();
+include 'includes/overall/header.php';
+?>
+<h1>Downloads</h1>
+<p>St. Thomas Aquinas Tutoring. </p>
+<p>Hello and Welcome. Click on the Register button to the right to Register now!</p>
+
+<?php include 'includes/footer.php'; ?>
+
