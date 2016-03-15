fb34453dbc0f0a0b1ef996ab2031773e8e74b710
Aryan Cheema <aryan.cheema.152@my.aquinas.me>
2016-03-14 23:55:23 -0400

    Update protected.php

--- includes.2/protected.php
+++ includes.2/protected.php
@@ -3,7 +3,7 @@ include 'core/init.php';
 include 'includes/overall/header.php';
 ?>
 <h1>Sorry you need to be logged in to do that!</h1>
-<p>Please register or log in by using the widget on the right. </p>
+<p>Please register or log in by using the widget on the right. </p
 
 
 
