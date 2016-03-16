c1dfbfbb42f91ee935af993a5213c30b77355ff8
Aryan Cheema <aryan.cheema.152@my.aquinas.me>
2016-03-15 23:46:30 -0400

    Update contact.php

--- includes.2/contact.php
+++ includes.2/contact.php
@@ -7,7 +7,7 @@ include 'includes/overall/header.php';
   <h1>Contact us.</h1>
 <p>
   <h1> Email: </h1>
-  <h1> Room number: </h1>
+  <h1> Room number </h1>
   <h1> Schedule </h1>
   <h1> Times of contact </h1>
   <h1> Levels of tutoring </h1>
